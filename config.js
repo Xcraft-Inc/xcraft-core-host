042335472e39d2876ad9e08f74c094d1e13b4e80
Samuel Loup <loup@epsitec.ch>
2016-08-10 09:09:53 +0200

    Add core-etc submodule

---
+++ config.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/**
+ * Retrieve the inquirer definition for xcraft-core-etc
+ */
+module.exports = [{
+  type: 'input',
+  name: 'mainQuest',
+  message: 'main quest to start',
+  default: 'defaultapp.start'
+}];
